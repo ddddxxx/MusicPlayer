0028c625a09ea0d341795b43a7f1973acbdf90af
Xander Deng <dengxiang2010@gmail.com>
2017-08-30 09:43:41 +0800

    add: MusicPlayer protocol

---
+++ Sources/MusicPlayer.swift
@@ -0,0 +1,76 @@
+//
+//  MusicPlayer.swift
+//
+//  This file is part of LyricsX
+//  Copyright (C) 2017  Xander Deng
+//
+//  This program is free software: you can redistribute it and/or modify
+//  it under the terms of the GNU General Public License as published by
+//  the Free Software Foundation, either version 3 of the License, or
+//  (at your option) any later version.
+//
+//  This program is distributed in the hope that it will be useful,
+//  but WITHOUT ANY WARRANTY; without even the implied warranty of
+//  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+//  GNU General Public License for more details.
+//
+//  You should have received a copy of the GNU General Public License
+//  along with this program.  If not, see <http://www.gnu.org/licenses/>.
+//
+
+import Cocoa
+import ScriptingBridge
+
+public enum MusicPlaybackState {
+    case stopped
+    case playing
+    case paused
+    case fastForwarding
+    case rewinding
+}
+
+public enum MusicRepeatMode {
+    case none
+    case one
+    case all
+}
+
+public enum MusicShuffleMode {
+    case off
+    case songs
+    case albums
+}
+
+protocol MusicPlyer {
+    
+    init()
+    
+    var playbackState: MusicPlaybackState { get }
+    var repeatMode: MusicRepeatMode { get set }
+    var shuffleMode: MusicShuffleMode { get set }
+    
+    var currentTrack: MusicTrack? { get }
+    var playerPosition: TimeInterval { get set }
+    
+    func play()
+    func pause()
+    func stop()
+    func skipToNext()
+    func skipToPrevious()
+    
+    var originalPlayer: SBApplication { get }
+}
+
+protocol MusicTrack {
+    
+    var id:     String { get }
+    var title:  String { get }
+    var album:  String? { get }
+    var artist: String? { get }
+    var duration: TimeInterval? { get }
+    var artwork:  NSImage? { get set }
+    var lyrics: String { get set }
+    var url:    URL? { get }
+    
+    var originalTrack: SBObject { get }
+}
